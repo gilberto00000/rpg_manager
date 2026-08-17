2d3d535e33004ac04bed0e491a5f5cefd2c01c62
gilberto00000 <gilbertomendesgm189@gmail.com>
2026-08-17 11:01:08 -0300

    ajustes para a criação de executaveis

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,14 @@
 module com.example.rpg_manager {
+
     requires javafx.controls;
     requires javafx.fxml;
+
     requires java.sql;
     requires javafx.graphics;
     requires java.desktop;
 
     opens com.example.rpg_manager.model to javafx.base;
     opens com.example.rpg_manager to javafx.fxml;
+
     exports com.example.rpg_manager;
 }
